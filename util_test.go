18ae986c201b709aade7c0fd2fb5736169cb68a2
2443591230@qq.com <19910818cz>
2018-07-09 21:34:11 +0800

    lb

--- util_test.go
+++ util_test.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"bytes"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -26,10 +29,25 @@ func TestBufPoolGet(t *testing.T) {
 func BenchmarkBufPool(b *testing.B) {
 	//b.SetBytes(10000)
 	b.ReportAllocs()
+	b.Log("1")
+	buf := BufPoolGet()
+	defer BufPoolFree(buf)
 	for i := 0; i < b.N; i++ {
-		buf := BufPoolGet()
-		buf.WriteString("hello")
-		buf.String()
-		BufPoolFree(buf)
+		buf.WriteString(`2018-05-25 14:43:34	4	6DE0755D5FDAF08CCF59859606B6D2DF	72503	gaid_122acc07-6e7e-4a6a-84a5-2d5c4bca0638		1		100022	460	zh_CN	Asia%2FShanghai	116.53042	39.930256	wifi	{"name":"Github","package":"com.seasonfif.github","version":"1.7.1","installtime":"1501554396221"}`)
+		buf.WriteString("\n")
 	}
+
+	ioutil.WriteFile("./tmp.log", buf.Bytes(), os.ModePerm)
+}
+func BenchmarkBuf2(b *testing.B) {
+	//b.SetBytes(10000)
+	b.ReportAllocs()
+	b.Log("1")
+	buf := bytes.NewBufferString("")
+	for i := 0; i < b.N; i++ {
+		buf.WriteString(`2018-05-25 14:43:34	4	6DE0755D5FDAF08CCF59859606B6D2DF	72503	gaid_122acc07-6e7e-4a6a-84a5-2d5c4bca0638		1		100022	460	zh_CN	Asia%2FShanghai	116.53042	39.930256	wifi	{"name":"Github","package":"com.seasonfif.github","version":"1.7.1","installtime":"1501554396221"}`)
+		buf.WriteString("\n")
+	}
+
+	ioutil.WriteFile("./tmp.log", buf.Bytes(), os.ModePerm)
 }
